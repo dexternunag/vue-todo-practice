5e92fb4199eb54d9b014965ad5fce13ef9064d68
dexternunag <nunagdexter@gmail.com>
2019-04-22 17:05:53 +0800

    Initial commit

---
+++ src/components/TodoItem.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="todo-item" :class="{'is-completed':todo.completed}">
+        <input type="checkbox" v-on:change="markComplete" />
+        <p>{{todo.title}}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'TodoItem',
+    props: [
+        'todo'
+    ],
+    methods: {
+        markComplete() {
+            console.log(123);
+        }
+    }
+}
+</script>
+
+<style scoped>
+.todo-item {
+    background: #f4f4f4;
+    padding: 10px;
+    border-bottom: 1px #ccc dotted;
+}
+
+.is-completed {
+    text-decoration: line-through;
+}
+
+.del {
+    background: #ff0000;
+    color: #fff;
+    border: none;
+    padding: 5px 9px;
+    border-radius: 50%;
+    cursor: pointer;
+    float: right;
+}
+</style>
+
---
+++ src/components/Todos.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <ul>
+            <li v-for="(todo, index) in todos" :key="index">
+                <TodoItem :todo="todo" />
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+import TodoItem from './TodoItem';
+
+export default {
+    name: 'Todos',
+    components: {
+        TodoItem
+    },
+    props: [
+        'todos'
+    ]
+}
+</script>
+
+<style scoped>
+
+</style>
